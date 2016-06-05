d4a04b9a21789f6c0e20faa8dcdfce550d594b53
fanqi <github@fanqi.xyz>
2016-06-05 21:39:40 +0800

    新建student Portlet

---
+++ docroot/WEB-INF/src/xyz/fanqi/liferay/demo/portlet/StudentPortlet.java
@@ -0,0 +1,11 @@
+package xyz.fanqi.liferay.demo.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class StudentPortlet
+ */
+public class StudentPortlet extends MVCPortlet {
+ 
+
+}
